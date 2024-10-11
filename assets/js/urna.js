e7d330607d22e21adca3b305fe8b8dd47f52f13d
Rafael <132296553+R4f4ell@users.noreply.github.com>
2024-10-11 16:42:06 -0300

    Responsividade em andamento

---
+++ assets/js/urna.js
@@ -0,0 +1,29 @@
+function digitar(numero) {
+    let tela = document.getElementById("tela");
+    if (tela.value.length < 2) { // Limita a entrada a 2 dígitos
+        tela.value += numero;
+    }
+}
+
+function corrigir() {
+    document.getElementById("tela").value = "";
+}
+
+function branco() {
+    document.getElementById("tela").value = "BRANCO";
+}
+
+function confirmar() {
+    let valor = document.getElementById("tela").value;
+    if (valor !== "") {
+        if(valor !== '10' && valor !== '20' && valor !== '30'){
+            alert("Digite o número correto de seu candidato")
+            corrigir()
+        } else {
+        alert("Voto confirmado: " + valor);
+        corrigir(); // Limpa a tela após confirmar
+        }
+    } else {
+        alert("Por favor, insira o número de seu candidato ou escolha branco.");
+    }
+}
